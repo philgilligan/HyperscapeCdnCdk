f9b2ddb4000d463be619b88c0006fb3b63041df2
Phil Gilligan <phil@agidea.uk>
2024-03-06 10:27:14 +0000

    Initial commit

---
+++ src/HyperscapeCdnCdk/HyperscapeCdnCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace HyperscapeCdnCdk
+{
+    public class HyperscapeCdnCdkStack : Stack
+    {
+        internal HyperscapeCdnCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
