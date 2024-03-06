2db70446baa5fcbea45ba37fec2d1a11813be00b
Phil Gilligan <phil@agidea.uk>
2024-03-06 16:15:43 +0000

    Added code to create S3 bucket and OAI for CloudFront

--- src/HyperscapeCdnCdk/HyperscapeCdnCdkStack.cs
+++ src/HyperscapeCdnCdk/HyperscapeCdnCdkStack.cs
@@ -1,4 +1,6 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.CloudFront;
+using Amazon.CDK.AWS.S3;
 using Constructs;
 
 namespace HyperscapeCdnCdk
@@ -7,7 +9,39 @@ namespace HyperscapeCdnCdk
     {
         internal HyperscapeCdnCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            // Create an S3 bucket for your content
+            var bucket = new Bucket(this, "hyperscape-cdn", new BucketProps
+            {
+                Versioned = true,
+                PublicReadAccess = false,
+                BlockPublicAccess = BlockPublicAccess.BLOCK_ALL
+            });
+
+            // Create an OAI for CloudFront
+            var oai = new OriginAccessIdentity(this, "HyperscapeCdnOai", new OriginAccessIdentityProps
+            {
+                Comment = "OAI for Hyperscape CDN CloudFront Distribution"
+            });
+
+            // Grant read access to the bucket from the OAI
+            bucket.GrantRead(oai);
+
+            // Create a CloudFront distribution that uses the OAI
+            new CloudFrontWebDistribution(this, "HyperscapeCdnDistribution", new CloudFrontWebDistributionProps
+            {
+                OriginConfigs = new ISourceConfiguration[]
+                {
+                    new SourceConfiguration
+                    {
+                        S3OriginSource = new S3OriginConfig
+                        {
+                            S3BucketSource = bucket,
+                            OriginAccessIdentity = oai
+                        },
+                        Behaviors = new IBehavior[] { new Behavior { IsDefaultBehavior = true } }
+                    }
+                }
+            });
         }
     }
 }
